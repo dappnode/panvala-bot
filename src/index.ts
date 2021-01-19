0ffe0a3368ba70ee0b901091e9b1e02983ff1745
Pablo <mendez4a@gmail.com>
2021-01-19 17:31:40 +0100

    parse file successfully

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,150 @@ import { discordToken } from "./params";
 import fs from "fs";
 import { Octokit } from "@octokit/rest";
 import { Endpoints } from "@octokit/types";
+import { compile, compileFromFile } from "json-schema-to-typescript";
+import {
+  ActionAlias,
+  ActionDistribution,
+  ActionIdentity,
+  Distribution,
+  PanvalaUser,
+  Receipt,
+} from "./types";
+import { parse } from "dotenv/types";
 
-type contentResponse = Endpoints["GET /repos/{owner}/{repo}/contents/{path}"]["response"];
+type ContentResponse = Endpoints["GET /repos/{owner}/{repo}/contents/{path}"]["response"];
 
 async function getFile() {
   const octokit = new Octokit();
   try {
-    const response: contentResponse = await octokit.repos.getContent({
+    const response: ContentResponse = await octokit.repos.getContent({
       owner: "dappnode",
       repo: "template-instance",
       path: "data/ledger.json",
     });
-    // @ts-ignore: Unreachable code error
-    const data = response.data.content;
-    //console.log(response.data.content);
-
-    const buff = Buffer.from(data, "base64");
-    const dataJson = buff.toString("ascii");
-    fs.writeFileSync("data.json", dataJson);
-    console.log(dataJson);
+    // Critical: content response type is wrong || version and date
+    const data = (response.data as any).content;
+    writeFile(data);
   } catch (e) {
     console.log(e);
   }
 }
 
-getFile();
+function writeFile(file: string) {
+  const buff = Buffer.from(file, "base64");
+  const dataJson = buff.toString("ascii");
+  fs.writeFileSync("data.json", dataJson);
+}
+
+function readFile() {
+  try {
+    const file = fs.readFileSync("./data.json", "utf-8");
+    const fileArr = file.trim().split("\n");
+    const fileJson = fileArr.map((line, i) => JSON.parse(line)); //.join("\n");
+    return fileJson;
+  } catch (e) {
+    // error: message, stack
+    e.message = `Error when reading data.json: ${e.message}`;
+    throw e;
+  }
+}
+
+function getReceipts() {
+  const file = readFile();
+  const distributions: ActionDistribution[] = file.filter((line) =>
+    instanceOfDistribution(line)
+  );
+  const allocations: Distribution[][] = distributions.map(
+    (distribution) => distribution.action.distribution.allocations
+  );
+
+  const receipts: Receipt[][][] = allocations.map((distributions) =>
+    distributions.map((distribution) => distribution.receipts)
+  );
+
+  const receiptsMerged = ([] as Receipt[][]).concat(...receipts);
+
+  return receiptsMerged;
+}
+
+function registerUser(user: PanvalaUser) {}
+
+function createUser({
+  discord,
+  address,
+}: {
+  discord: string;
+  address: string;
+}): PanvalaUser {
+  const id = getId(discord);
+
+  if (id) {
+    // User exists
+    const grain = getGrain(id);
+    return {
+      id: id,
+      address: address,
+      discord: discord,
+      grain: grain,
+    };
+  } else {
+    // User does not exist
+    throw Error("User was not found");
+  }
+}
+
+function getGrain(id: string) {
+  const receipts: Receipt[][] = getReceipts();
+  const distributions = receipts.map((receipt) =>
+    receipt.filter((distribution) => distribution.id === id)
+  );
+  const distributionsMerged: Receipt[] = ([] as Receipt[]).concat(
+    ...distributions
+  );
+  const totalUserGrain = distributionsMerged.reduce(
+    (a, b): Receipt => {
+      return {
+        amount: (parseInt(a.amount) + parseInt(b.amount)).toString(),
+        id: id,
+      };
+    }
+  );
+  return totalUserGrain.amount;
+}
+
+const grain = getGrain("swQm7i0bz6wumzAs8ZNteQ");
+console.log(grain);
+
+function instanceOfIdentity(
+  object: ActionAlias | ActionIdentity | ActionDistribution
+): object is ActionIdentity {
+  return (object as ActionIdentity).action.identity !== undefined;
+}
+
+function instanceOfAlias(
+  object: ActionAlias | ActionIdentity | ActionDistribution
+): object is ActionAlias {
+  return (object as ActionAlias).action.alias !== undefined;
+}
+
+function instanceOfDistribution(
+  object: ActionAlias | ActionIdentity | ActionDistribution
+): object is ActionDistribution {
+  return (object as ActionDistribution).action.distribution !== undefined;
+}
+
+function getId(discord: string): string | null {
+  const file = readFile();
+  const userCreation: ActionIdentity = file.find((line) =>
+    instanceOfIdentity(line)
+      ? line.action.identity.name === discord
+        ? line.action.identity.id
+        : null
+      : null
+  );
+  if (userCreation) return userCreation.action.identity.id;
+  return null;
+}
 
 /* const client = new Discord.Client();
 client.login(discordToken);
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,64 @@
 export interface PanvalaUser {
   discord: string;
-  address: string;
+  id: string;
   grain: string;
+  address: string;
+}
+
+export interface ActionIdentity {
+  action: {
+    identity: {
+      address: string; //"N\u0000sourcecred\u0000core\u0000IDENTITY\u0000tPCdsEqzToKOaHMjzuMY4w\u0000"
+      aliases: [];
+      id: string; // "tPCdsEqzToKOaHMjzuMY4w"
+      name: string; //"Trach"
+      subtype: string; //"USER" | "PROJECT"
+    };
+    type: string; //"CREATE_IDENTITY"
+  };
+  ledgerTimestamp: number; //1607965089679
+  uuid: string; //"nllRk3VkFDO4wPUemSMgvg"
+  version: string; //"1"
+}
+
+export interface ActionAlias {
+  action: {
+    alias: {
+      address: string; //"N\u0000sourcecred\u0000discord\u0000MEMBER\u0000user\u000094400988516995072\u0000";
+      description: string; //"discord/Trach#0842"
+    };
+    identityId: string; //"tPCdsEqzToKOaHMjzuMY4w"
+    type: string; //"ADD_ALIAS"
+  };
+  ledgerTimestamp: number; //1607965089679
+  uuid: string; //"uoyca75i7IMeaHwjjml2Ww"
+  version: string; //"1"
+}
+
+export interface Receipt {
+  amount: string; // "347884681285057728";
+  id: string; //"tPCdsEqzToKOaHMjzuMY4w"
+}
+
+export interface Distribution {
+  id: string; //"Zj0IW04OpVFWfKdpZxYrtQ"
+  policy: {
+    budget: string; // "2500000000000000000000"
+    policyType: string; //"IMMEDIATE"
+  };
+  receipts: Receipt[];
+}
+
+export interface ActionDistribution {
+  action: {
+    distribution: {
+      allocations: Distribution[];
+      credTimestamp: number; // 1607817600000
+      id: string; //"g5VfLfe8riafxIasJLNHvg"
+    };
+    type: string; //"DISTRIBUTE_GRAIN"
+  };
+  ledgerTimestamp: number; // 1607979812883
+  uuid: string; // "bSPLDEka9GI9BIPidyn6qQ"
+  version: string; //"1"
 }
